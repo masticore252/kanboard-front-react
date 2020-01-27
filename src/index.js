c0131a9a00a25e15f4ec4519571f6b87c5be1239
Albert Vásquez <masticore252@gmail.com>
2020-01-27 00:59:46 -0300

    Add redux-thunk

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import reducers from './redux/reducers'
+import thunk from 'redux-thunk';
 
 const store = createStore(
     reducers,
+    applyMiddleware(thunk)
 )
 
 ReactDOM.render(
